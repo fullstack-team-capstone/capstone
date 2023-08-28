3fabc5b74b505dea56e458d1f3764fd5ca672fbd
Michael <mschiff327@gmail.com>
2023-08-28 13:15:58 -0600

    funny

--- capstone/src/client/components/Userpage.jsx
+++ capstone/src/client/components/Userpage.jsx
@@ -4,6 +4,9 @@ const Userpage = () => {
   return (
     <div>
       <h1>Shalom!</h1>
+      <div dangerouslySetInnerHTML={{
+          __html: '<iframe src="https://giphy.com/embed/3o6Mb9iBZvwmYeSQZW" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/thesimpsons-3o6Mb9iBZvwmYeSQZW">via GIPHY</a></p>'
+      }}></div>
       <p>This is a boilerplate React JSX component.</p>
     </div>
   );
