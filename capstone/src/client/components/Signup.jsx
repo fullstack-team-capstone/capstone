caf4a9048fec2136ddb991a49d2d3374e2ff9c1c
Michael <mschiff327@gmail.com>
2023-08-28 10:18:38 -0600

    boilerplate code for componenets

---
+++ capstone/src/client/components/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Signup = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a boilerplate React JSX component.</p>
+    </div>
+  );
+};
+
+export default Signup;
---
+++ capstone/src/client/components/Singleitem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Singleitem = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a boilerplate React JSX component.</p>
+    </div>
+  );
+};
+
+export default Singleitem;
