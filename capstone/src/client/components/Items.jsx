2f7b2259730db70e04bf2d00aecc9ecbbe2bff59
davidilejeune <davidilejeune@aol.com>
2023-08-30 10:48:08 -0400

    Added feature for star rating of each item to appear in card

--- capstone/src/client/components/Items.jsx
+++ capstone/src/client/components/Items.jsx
@@ -7,15 +7,18 @@ import Col from "react-bootstrap/Col"
 
 function Items() {
   const [products, setProducts] = useState([])
+  const [reviews, setReviews] = useState([])
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-      const response = await fetch('http://localhost:3000/api/items')
-      console.log(response)
-      const data = await response.json()
+      let response = await fetch('http://localhost:3000/api/items')
+      let data = await response.json()
       setProducts(data.items)
-      console.log(data.items)
+      response = await fetch('http://localhost:3000/api/reviews')
+      data = await response.json()
+      setReviews(data.reviews)
+      console.log(data.reviews)
     } catch (error) {
       console.error (error)
     }
@@ -24,26 +27,50 @@ function Items() {
   }, []
   )
 
+  const getReviewsByReviewableId = (reviewableId) => {
+    try {
+      const reviewsForItem = reviews.filter(review => review.reviewableid === reviewableId)
+      console.log('filtered reviews for item', reviewsForItem)
+      return reviewsForItem
+
+    } catch (error) {
+      console.error (error)
+    }
+  }
+
+  const calculateAverageRating = (reviews) => {
+    if (reviews.length === 0) return "Be the first to review"
+    const total = reviews.reduce ((sum, review) => sum + review.stars, 0)
+    const average = Math.round(total/reviews.length) 
+    console.log('Average', average)
+    return '\u2B50'.repeat(average)
+  }
+
+  const fetchCalculate = (id) => {
+    const reviews = getReviewsByReviewableId(id)
+    const averageRating = calculateAverageRating(reviews)
+    console.log("Type of average rating:", typeof averageRating)
+    console.log(averageRating)
+    return averageRating
+  }
 
   return (
     <Container>
       <Row>
         {products.map((product) => { 
+          
           return (
-            <Col>
+            <Col key={product.id}>
                 <Card style={{ width: '18rem' }}>
                   <Card.Img variant="top" src={product.imageurl} />
                   <Card.Body>
                     <Card.Title>{product.itemname}</Card.Title>
-                    <Card.Text>
-                      {product.description}
-                    </Card.Text>
                   </Card.Body>
                   <ListGroup className="list-group-flush">
-                    <ListGroup.Item>***</ListGroup.Item>
+                    <ListGroup.Item>{fetchCalculate(product.id)}</ListGroup.Item> 
                   </ListGroup>
                   <Card.Body>
-                    <Card.Link href="#">Single Item View</Card.Link>
+                    <Card.Link href="http://localhost:3000/items">View Item</Card.Link>
                   </Card.Body>
                 </Card>
             </Col>
